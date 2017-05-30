808b6bd9be4eb4457ae4e85609ef2eda1f12704d
Unknown <cebulla.christopher@web.de>
2017-05-30 14:41:07 +0200

    marker try

--- search.js
+++ search.js
@@ -7,60 +7,9 @@ var $fabi = $('#fab--i');
 var $input = $('#input');
 var $rangeslider = $('#range');
 var $btnMore = $('#btn-more');
-var currentDay = "23";
 
 var search_points = L.layerGroup();
 //var allPoints;
-
-$rangeslider.on("input", function () {
-    getCorrectLayer(this.value);
-    currentDay = this.value;
-});
-
-function getCorrectLayer(value) {
-    switch (value) {
-        case "23":
-            removeAllLayers();
-            p23.addTo(map);
-            break;
-        case "24":
-            removeAllLayers();
-            p24.addTo(map);
-            break;
-        case "25":
-            removeAllLayers();
-            p25.addTo(map);
-            break;
-        case "26":
-            removeAllLayers();
-            p26.addTo(map);
-            break;
-        case "27":
-            removeAllLayers();
-            p27.addTo(map);
-            break;
-        case "28":
-            removeAllLayers();
-            p28.addTo(map);
-            break;
-        case "29":
-            removeAllLayers();
-            p29.addTo(map);
-            break;
-        default:
-            break;
-    }
-}
-
-function removeAllLayers() {
-    map.removeLayer(p23);
-    map.removeLayer(p24);
-    map.removeLayer(p25);
-    map.removeLayer(p26);
-    map.removeLayer(p27);
-    map.removeLayer(p28);
-    map.removeLayer(p29);
-}
 
 var bMore = false;
 $btnMore.click(function(e) {
@@ -92,7 +41,7 @@ $searchTrigger.click(function (e) {
 });
 
 $searchReset.click(function (e) {
-    getCorrectLayer(currentDay);
+    getCorrectLayer(activities));
     $searchOverlay.fadeOut(500);
     $fabi.text("search");
     $input.val("");
@@ -101,7 +50,7 @@ $searchReset.click(function (e) {
 });
 
 function resetMap() {
-    removeAllLayers();
+    map.removeLayer(activities);
 }
 
 $input.keypress(function (e) {
@@ -115,8 +64,8 @@ function searchPoints(searchString) {
     map.removeLayer(search_points);
     search_points = L.layerGroup();
     var searchCount = 0;
-    var specificPoints= $.ajax({ 
-      url: currentDay + '.json', 
+    var specificPoints= $.ajax({
+      url:'activities.json',
       async: false
    }).responseJSON;
     resetMap();
@@ -130,15 +79,6 @@ function searchPoints(searchString) {
         }
     }
 
-    if (searchString.includes('aachen') || searchString.includes('Aachen')) {
-        map.setView([50.776, 6.082], 13);
-        if (searchCount == 0) {
-            $searchOverlay.fadeOut(500);
-            $fabi.text("search");
-            b = false;
-            return;
-        }
-    }
     $searchOverlay.fadeOut(500);
     $fabi.text("search");
     b = false;
