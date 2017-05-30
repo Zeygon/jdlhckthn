6a472443629ec08db1a0959ab8d4313f25019334
Unknown <cebulla.christopher@web.de>
2017-05-30 19:04:42 +0200

    Tag Suche implementiert

--- app.js
+++ app.js
@@ -15,7 +15,10 @@ $.getJSON('activities.json', function (d){
   data = d.activities;
   dataToMap();
 })
+
   console.log("own point");
+
+
 var dataToMap = function(){
     console.log("datatomap");
   for (var i = 0;i<data.length;i++){
